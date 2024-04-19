eee2abe56198e0526cb697db1d57b344ef726361
licjorgetoloza <jorgetoloza.dg.ok@gmail.com>
2024-04-18 22:07:21 -0300

    correcciones storage

--- js/main.js
+++ js/main.js
@@ -1,82 +1,9 @@
-const servicios = [
-
-        { 
-        nombre: "Diseño de Marca", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 100000, 
-        pack: "Premium",
-    },
-        { 
-        nombre: "Diseño de Marca", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 90000, 
-        pack: "Advance",
-    },
-        { 
-        nombre: "Diseño de Marca", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 80000, 
-        pack: "Basic",
-    },
-        { 
-        nombre: "Diseño de Piezas Graficas", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 105000, 
-        pack: "Premium",
-    },
-        { 
-        nombre: "Diseño de Piezas Graficas", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 95000, 
-        pack: "Advance",
-    },
-        { 
-        nombre: "Diseño de Piezas Graficas", 
-        areaServicio: "Diseño Gráfico",  
-        precio: 85000, 
-        pack: "Basic",
-    },
-        { 
-        nombre: "Admin Redes Soc", 
-        areaServicio: "Marketing Digital",  
-        precio: 95000, 
-        pack: "Premium",
-    },
-        { 
-        nombre: "Admin Redes Soc", 
-        areaServicio: "Marketing Digital",  
-        precio: 85000, 
-        pack: "Advance",
-    },
-        { 
-        nombre: "Admin Redes Soc", 
-        areaServicio: "Marketing Digital",  
-        precio: 75000, 
-        pack: "Basic",
-    },
-        { 
-        nombre: "Crear Contenido y Publicar", 
-        areaServicio: "Marketing Digital",  
-        precio: 120000, 
-        pack: "Premium",
-    },
-        { 
-        nombre: "Crear Contenido y Publicar", 
-        areaServicio: "Marketing Digital",  
-        precio: 110000, 
-        pack: "Advance",
-    },
-        { 
-        nombre: "Crear Contenido y Publicar", 
-        areaServicio: "Marketing Digital",  
-        precio: 99000, 
-        pack: "Basic",
-    }
-];
+const servicios = ["Diseño de Marca A DG", "Diseño de Marca B DG", "Diseño de Marca C DG",
+"Diseño de Piezas Graficas A DG", "Diseño de Piezas Graficas B DG", "Diseño de Piezas Graficas C DG",
+"Admin Redes Soc A MD", "Admin Redes Soc B MD", "Admin Redes Soc C MD"];
 
 const contenedor = document.querySelector ("div");
 
-console.log(contenedor);
 
 let lista2 = document.createElement("ul");
 lista2.classList.add("lista2");
@@ -103,52 +30,37 @@ agregaServAlForm.addEventListener("submit", agregaServ)
         }
 
     agregaServAlForm.reset();
- }
-
-/*for(servicio of servicios) {
-       lista.innerHTML+=`<li class=lista-item>${servicio}</li>`;
  };
- contenedor.append(lista);*/
 
 
- /*const disenioGrafico = document.querySelector (".ul");
 
-let lista = document.createElement("li");
-lista.classList.add("lista");
+ const registroForm = document.querySelector('#registroForm');
+    registroForm.addEventListener('submit', (e)=>{
+        e.preventDefault()
+
+        const name = document.querySelector('#name').value
+        const email = document.querySelector('#email').value
+        const password = document.querySelector('#password').value
+
+        const Users = JSON.parse(localStorage.getItem('user')) || []
+        const usuarioRegistrado = Users.find (user => user.email === email)
+
+        if (usuarioRegistrado){
+            return alert ('Ud. ya está Registrado')
+        } 
+
+        Users.push ({name: name, email: email, password: password})
+        localStorage.setItem('Users', JSON.stringify(Users))
+
+            alert("Registro Exitoso")
+
+    });
 
 
-disenioGrafico.append;*/
 
 
- //Lo copié tal cual del ejercicio de la presentacion de la clase y no me funciona
- //no encuentro el error.
- 
- /*for(servicio of servicios) {
-   let contenedor = document.createElement ("ul");
-      contenedor.innerHTML=`<h4> Propducto ${servicio.producto}</h4>
-                            <p> Area ${servicio.areaServicio}</p>
-                            <p> Cliente ${servicio.clienteTipo}</p>
-                            <p> Pack ${servicio.pack}</p>
-                            <b> Precio ${servicio.precio}</b>`;
-      ul.appendChild(contenedor);
-}
-*/
+        
+    
 
-let usuarioIngresado = input (nameUser);
-localStorage.setItem("usuario-ingresado", usuarioIngresado);
 
-const formulario=document.querySelector("#formulario");
-console.log(formulario);
-
-function mostrarError = document.createElement("p");
-mensajeError.textContent = msg
-mensajeError.classList.add("error")
-const modal = document.querySelector("#content")
-modal.appendChild(mensajeError);
-
-function agregarcomentario(evt) {
-    evt.preventDefault()
-    console.log("Agregaste un comentario")
-    const comentario = document.querySelector("#comentario").value
-}
-
+      
